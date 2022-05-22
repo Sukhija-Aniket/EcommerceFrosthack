7e816e8f77eb8638e5e140dcbbbade716e33374b
Kuldeep Jain <kuldipjain2002@gmail.com>
2022-05-22 11:29:41 +0530

    made changes in models

--- core/models.py
+++ core/models.py
@@ -12,10 +12,20 @@ CATEGORY_CHOICES = (
     ('OW', 'OutWear')
 )
 
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+
+
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
+    category = models.CharField(choices = CATEGORY_CHOICES ,max_length=2, default = 'S' )
+    label = models.CharField(choices = LABEL_CHOICES ,max_length=1 ,default = 'P' )
 
     def __str__(self):
         return self.title
